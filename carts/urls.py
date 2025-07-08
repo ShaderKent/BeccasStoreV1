76591465888e4317c98934ac592621289c7eaa47
Kent <Kent>
2025-07-08 15:00:04 -0400

    Fixed issue with adding and removing products in cart.
    Fixed minor visuasl issues:
        -logo icon is placed correctly for mobile
        -Spacing for cart item values fixed.

--- carts/urls.py
+++ carts/urls.py
@@ -5,10 +5,9 @@ from carts import views
 app_name = "carts"
 
 urlpatterns = [
-    # path("", views.CartView.as_view(), name="cart"),
     path("", views.cart, name="cart"),
     path("add_cart/<int:product_id>/", views.add_cart, name="add_cart"),
-    path("remove_cart/<int:product_id>/", views.remove_cart, name="remove_cart"),
-    path("delete_cart/<int:product_id>/", views.delete_cart, name="delete_cart"),
+    path("remove_cart/<int:product_id>/<int:cart_item_id>/", views.remove_cart, name="remove_cart"),
+    path("delete_cart/<int:product_id>/<int:cart_item_id>/", views.delete_cart, name="delete_cart"),
     path("clear_cart/", views.clear_cart, name="clear_cart"),
 ]
--- carts/views.py
+++ carts/views.py
@@ -97,22 +97,28 @@ def add_cart(request, product_id):
     #Sends user to their cart page
     return redirect("carts:cart")
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product  = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect("carts:cart")
 
-def delete_cart(request, product_id):
+def delete_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product  = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        cart_item.delete()
+    except:
+        pass
     return redirect("carts:cart")
 
 def clear_cart(request):
